ca83af9650563a234d47a118b1cb71b0e52a19c6
Hankkin <1019283569@qq.com>
2016-04-25 09:49:19 +0800

    更新优化崩溃bug

--- library/src/main/java/com/hankkin/library/SwipeMenuLayout.java
+++ library/src/main/java/com/hankkin/library/SwipeMenuLayout.java
@@ -83,9 +83,12 @@ public class SwipeMenuLayout extends FrameLayout {
             @Override
             public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
                 // TODO
-                if ((e1.getX() - e2.getX()) > MIN_FLING && velocityX < MAX_VELOCITYX) {
-                    isFling = true;
+                if (e1 != null && e2 != null){
+                    if ((e1.getX() - e2.getX()) > MIN_FLING && velocityX < MAX_VELOCITYX) {
+                        isFling = true;
+                    }
                 }
+
                 // Log.i("byz", MAX_VELOCITYX + ", velocityX = " + velocityX);
                 return super.onFling(e1, e2, velocityX, velocityY);
             }
